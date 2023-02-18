aeeb98afc832ae1c6866e59d3072e472132b694b
Danny Boy <orienta.dan@gmail.com>
2023-02-18 13:11:58 +0900

    bugfix - remove errors as admin in /hymn/2277/CrystalSon

--- resources/views/layouts/partials/add_hymn_form.blade.php
+++ resources/views/layouts/partials/add_hymn_form.blade.php
@@ -9,6 +9,7 @@
                         <i class="far fa-question-circle"></i>
                     </a>
                 </div>
+                @if(!is_null($hymn))
                 @foreach ($userHinarios as $userHinario)
                     <form class="add_hymn_{{ $hymn->id }}_to_{{ $userHinario->id }}" role="form" method="POST" action="{{ url('/add-hymn-to-user-hinario') }}" accept-charset="UTF-8" id="add-hymn-{{ $hymn->id }}-to-user-hinario-{{ $userHinario->id }}">
                     <div class="form-group">
@@ -38,6 +39,7 @@
                         });
                     </script>
                 @endforeach
+                @endif
 
                 <div class="help-block text-center">
                 @if (count($userHinarios) > 0)
@@ -47,35 +49,37 @@
                 @endif
                 </div>
 
-                <form class="add_hymn_{{ $hymn->id }}_to_new" role="form" method="POST" action="{{ url('/add-hymn-to-user-hinario') }}" accept-charset="UTF-8" id="add-hymn-{{ $hymn->id }}-to-user-hinario">
-                    <div class="form-group">
-                            @csrf
-                            <label class="sr-only" for="new_hinario_name">{{ __('universal.add_hymn_form.new_hinario_name') }}</label>
-                            <input name="new_hinario_name" type="text" class="new_hinario_name add-hymn-text-input" id="new_hinario_name" placeholder="{{ __('universal.add_hymn_form.new_name_placeholder') }}">
-                            <input type="hidden" name="hymn_id" value="{{ $hymn->id }}" class="hymn_id">
-                            <input type="hidden" name="action" value="create" class="action">
-                            <button type="submit" class="btn btn-primary btn-block add-hymn-button">{{ __('universal.add_hymn_form.create_new_hinario') }}</button>
-                    </div>
-                    <div class="add_hymn_{{ $hymn->id }}_to_new_response add-hymn-response" id="add_hymn_{{ $hymn->id }}_to_new_response"></div>
-                </form>
+                @if(!is_null($hymn))
+                    <form class="add_hymn_{{ $hymn->id }}_to_new" role="form" method="POST" action="{{ url('/add-hymn-to-user-hinario') }}" accept-charset="UTF-8" id="add-hymn-{{ $hymn->id }}-to-user-hinario">
+                        <div class="form-group">
+                                @csrf
+                                <label class="sr-only" for="new_hinario_name">{{ __('universal.add_hymn_form.new_hinario_name') }}</label>
+                                <input name="new_hinario_name" type="text" class="new_hinario_name add-hymn-text-input" id="new_hinario_name" placeholder="{{ __('universal.add_hymn_form.new_name_placeholder') }}">
+                                <input type="hidden" name="hymn_id" value="{{ $hymn->id }}" class="hymn_id">
+                                <input type="hidden" name="action" value="create" class="action">
+                                <button type="submit" class="btn btn-primary btn-block add-hymn-button">{{ __('universal.add_hymn_form.create_new_hinario') }}</button>
+                        </div>
+                        <div class="add_hymn_{{ $hymn->id }}_to_new_response add-hymn-response" id="add_hymn_{{ $hymn->id }}_to_new_response"></div>
+                    </form>
 
-                <script>
-                    //Feedback form processing
-                    jQuery('.add_hymn_{{ $hymn->id }}_to_new').on('submit', function( e ) {
-                        e.preventDefault();
-                        var $form = jQuery(this);
-                        // update user interface
-                        $form.find('.add_hymn_{{ $hymn->id }}_to_new_response').html('<span><i class="fas fa-ellipsis-h"></i></span>');
-                        // Prepare query string and send AJAX request
-                        jQuery.ajax({
-                            url: '/add-hymn-to-user-hinario',
-                            data: 'ajax=true&hymn_id=' + escape($form.find('.hymn_id').val()) + '&action=create&new_hinario_name=' + escape($form.find('.new_hinario_name').val()),
-                            success: function(msg) {
-                                $form.find('.add_hymn_{{ $hymn->id }}_to_new_response').html(msg);
-                            }
+                    <script>
+                        //Feedback form processing
+                        jQuery('.add_hymn_{{ $hymn->id }}_to_new').on('submit', function( e ) {
+                            e.preventDefault();
+                            var $form = jQuery(this);
+                            // update user interface
+                            $form.find('.add_hymn_{{ $hymn->id }}_to_new_response').html('<span><i class="fas fa-ellipsis-h"></i></span>');
+                            // Prepare query string and send AJAX request
+                            jQuery.ajax({
+                                url: '/add-hymn-to-user-hinario',
+                                data: 'ajax=true&hymn_id=' + escape($form.find('.hymn_id').val()) + '&action=create&new_hinario_name=' + escape($form.find('.new_hinario_name').val()),
+                                success: function(msg) {
+                                    $form.find('.add_hymn_{{ $hymn->id }}_to_new_response').html(msg);
+                                }
+                            });
                         });
-                    });
-                </script>
+                    </script>
+                @endif
             </div>
         </div>
     </li>
