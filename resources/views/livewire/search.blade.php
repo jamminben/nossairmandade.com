938fde20063260bef23355f43a4751934b74c5d1
Danny Boy <orienta.dan@gmail.com>
2023-02-21 07:24:45 +0900

    bugfix: getslug

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -48,10 +48,10 @@
                 <div class="hymn-list-number">{{ $loop->iteration }}</div>
                 <div class="inline-block bottompadding_20">
                     <div class="hymn-list-name">
-                        <a href="{{ url($hymnTranslation->hymn->getSlug()) }}">{{ mb_convert_case($hymnTranslation->name, MB_CASE_TITLE, "UTF-8") }}</a>
+                        <a href="{{ !is_null($hymnTranslation->hymn) ? url( $hymnTranslation->hymn->getSlug()) : null }}">{{ mb_convert_case($hymnTranslation->name, MB_CASE_TITLE, "UTF-8") }}</a>
                     </div>
                     <p>
-                        {{ $hymnTranslation->hymn->receivedBy->display_name }}
+                        {{ !is_null($hymnTranslation->hymn) ?$hymnTranslation->hymn->receivedBy->display_name : null }}
                         @if (!empty($hymnTranslation->hymn->receivedHinario))
                             - {{ $hymnTranslation->hymn->receivedHinario->getName() }} #{{ $hymnTranslation->hymn->received_order }}
                         @endif
